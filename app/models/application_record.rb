662408ca2492d20d7fb70868671362aad43b7264
Orban Botond <orbanbotond@gmail.com>
2018-12-20 00:33:25 +0200

    Initial commit: Rails base

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,3 @@
-# app/models/application_record.rb
-# the presence of this file prevents rails migrations from recreating application_record.rb
-# see https://github.com/rails/rails/issues/29407
-
-require 'models/application_record.rb'
+class ApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
+end
