e44b899fb34b212e7d96bdf122748a163fa48ac9
Orban Botond <orbanbotond@gmail.com>
2018-12-20 19:27:04 +0200

    The state is introduced!

--- app/hyperstack/components/edit_item.rb
+++ app/hyperstack/components/edit_item.rb
@@ -1,10 +1,19 @@
 class EditItem < HyperComponent
   param :todo
 
+  triggers :saved
+  triggers :cancel
+  after_mount { DOM[dom_node].focus }
+
   render do
     INPUT(defaultValue: @Todo.title)
     .on(:enter) do |evt|
       @Todo.update(title: evt.target.value)
+      saved! 
     end
+    .on(:blur) do |evt|
+      cancel!
+    end
+
   end
 end
--- app/hyperstack/components/todo_item.rb
+++ app/hyperstack/components/todo_item.rb
@@ -2,9 +2,15 @@ class TodoItem < HyperComponent
   param :todo
 
   render(LI) do
-    INPUT(type: :checkbox, checked: @Todo.completed)
-    .on(:change) { @Todo.update(completed: !@Todo.completed) }
-    EditItem(todo: @Todo)
-    A { ' -X-' }.on(:click) { @Todo.destroy }
+    if(@editing)
+      EditItem(todo: @Todo)
+      .on(:cancel,:saved) { mutate @editing = false }
+    else
+      INPUT(type: :checkbox, checked: @Todo.completed)
+      .on(:change) { @Todo.update(completed: !@Todo.completed) }
+      LABEL{ @Todo.title}
+      .on(:click) { mutate @editing = true }
+      A { ' -X-' }.on(:click) { @Todo.destroy }
+    end
   end
 end
