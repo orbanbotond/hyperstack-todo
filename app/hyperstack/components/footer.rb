3180a1a724d137f67171c505294a113404348713
Orban Botond <orbanbotond@gmail.com>
2018-12-20 18:44:51 +0200

    The switching between active and completed just works well...!

--- app/hyperstack/components/footer.rb
+++ app/hyperstack/components/footer.rb
@@ -1,5 +1,13 @@
 class Footer < HyperComponent
+  include Hyperstack::Router::Helpers
+
+  def link_item(path)
+    NavLink("/#{path}", style: { marginRight: 10 }) { path.camelize }
+  end
+
   render(DIV) do
-    'Footer will go here'
+    link_item(:all)
+    link_item(:active)
+    link_item(:completed)
   end
 end
