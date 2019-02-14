80962558be911dce1e2284cc9230748aca6cf3e2
Orban Botond <orbanbotond@gmail.com>
2019-02-14 19:02:44 +0200

    Working well..

--- app/hyperstack/stores/todo_list.rb
+++ app/hyperstack/stores/todo_list.rb
@@ -1,24 +1,24 @@
 class TodoList
   include Hyperstack::State::Observable
 
-  receives AddItem do 
-    all << params.todo
-  end
-
-  class << self
-    state_accessor :all do
-      @all ||= Todo.all
-    end
-  end
+  # receives AddItem do 
+  #   all << params.todo
+  # end
 
   # class << self
-  #   mutator :add do |todo|
-  #     all << todo
-  #   end
-
-  #   observer :all do
-  #     # @all ||= Todo.send(match.params[:scope])
+  #   state_accessor :all do
   #     @all ||= Todo.all
   #   end
   # end
+
+  class << self
+    mutator :add do |todo|
+      all << todo
+    end
+
+    observer :all do
+      # @all ||= Todo.send(match.params[:scope])
+      @all ||= Todo.all
+    end
+  end
 end
