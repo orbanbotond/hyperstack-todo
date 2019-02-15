f8b881795337c85dab1839e16fc4ca9dc28649e7
Orban Botond <orbanbotond@gmail.com>
2019-02-15 23:09:47 +0200

    The seeds are ok.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create email: "boti@gmail.com", 
+                first_name: "Boti",
+                last_name: "Orban",
+                password: 'pokolgep',
+                password_confirmation: 'pokolgep'
+
+o1 = Organization.create name: 'Logic Optimum',
+                        description: 'A One Person RoR shop'
+
+o2 = Organization.create name: '<Epam>',
+                        description: 'Central Europes Largest Software Solution Provider'
+
+o1.users << user
+
