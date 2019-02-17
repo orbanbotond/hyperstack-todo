ce79034e87263132e394a701bc1f39c99a663b2a
Orban Botond <orbanbotond@gmail.com>
2019-02-17 23:27:54 +0200

    The organization and the membership is saved in an operation.

--- app/hyperstack/models/application_record.rb
+++ app/hyperstack/models/application_record.rb
@@ -1,3 +1,13 @@
+module ActiveRecord
+  module InstanceMethods
+    alias save_before_patch save
+
+    def save(*args, &block)
+      save_before_patch(*args, &block).then { |r| r }
+    end
+  end
+end if RUBY_ENGINE=='opal'
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   regulate_scope all: false
--- app/hyperstack/operations/organizations/create_operation.rb
+++ app/hyperstack/operations/organizations/create_operation.rb
@@ -5,31 +5,15 @@ module Organizations
     param :description, nils: true
 
     step do
-      @org_promise = Organization.create name: params.name, 
-                                description: params.description
+      Organization.create name: params.name, 
+                          description: params.description
     end
 
-    step do
-      @org_promise.then do |result|
-        if result[:success]
-          Membership.create user_id: params.user.id,
-                            organization_id: result[:models].first.id
-        end
+    step do |org|
+      if org[:success]
+        Membership.create user_id: params.user.id, 
+                          organization_id: org[:models].first.id
       end
     end
-
-    # step do
-    #   Organization.create name: params.name, 
-    #                       description: params.description
-    # end
-
-    # step do |org|
-    #   puts org.nil?
-    #   puts org
-    #   if org[:success]
-    #     Membership.create user_id: params.user.id, 
-    #                       organization_id: org[:models].first.id
-    #   end
-    # end
   end
 end
